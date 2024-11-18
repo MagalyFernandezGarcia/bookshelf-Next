51d920cd11dc21fbdbf4ea3a44fcf5050325a476
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-11-18 16:03:45 +0100

    adding modal on udatePage

--- components/FormUpdate.tsx
+++ components/FormUpdate.tsx
@@ -13,6 +13,7 @@ import upCat from "@/images/upCat.png";
 import check from "@/images/check-solid.svg";
 import eraser from "@/images/eraser-solid.svg";
 import { zodResolver } from "@hookform/resolvers/zod";
+import ConfirmModal from "./Modals/ConfirmModal";
 
 const FormUpdate = ({
 	currentBook,
@@ -47,6 +48,7 @@ const FormUpdate = ({
 	});
 
 	const [resetState, setResetState] = useState(0);
+	const [showModal, setShowModal] = useState(false);
 	const onSubmit: SubmitHandler<BookData> = async (data) => {
 		if (data.borrower && !data.date) {
 			data.date = new Date().toISOString();
@@ -55,6 +57,7 @@ const FormUpdate = ({
 		const { success, data: validatedBook } = BookSchema.safeParse(data);
 
 		if (success) {
+			setShowModal(true);
 			try {
 				await updateBook(validatedBook, currentBook.id);
 				setResetState(+1);
@@ -65,80 +68,94 @@ const FormUpdate = ({
 			console.log("oups");
 		}
 	};
+	const handleModal = () => {
+		setShowModal(false);
+	};
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<InputStyle labelTxt="title" register={register} registerName="title" />
-			<section className="flex gap-4">
-				<div>
+	if (showModal) {
+		return <ConfirmModal onSetModal={handleModal} />;
+	}
+	if (!showModal) {
+		return (
+			<>
+				<form onSubmit={handleSubmit(onSubmit)}>
 					<InputStyle
-						labelTxt="author"
+						labelTxt="title"
 						register={register}
-						registerName="author"
+						registerName="title"
 					/>
-					<InputStyle
-						labelTxt="genre"
+					<section className="flex gap-4">
+						<div>
+							<InputStyle
+								labelTxt="author"
+								register={register}
+								registerName="author"
+							/>
+							<InputStyle
+								labelTxt="genre"
+								register={register}
+								registerName="genre"
+							/>
+						</div>
+						<div>
+							<InputStyle
+								labelTxt="volume"
+								register={register}
+								registerName="volume"
+							/>
+							<InputStyle
+								labelTxt="serie"
+								register={register}
+								registerName="serie"
+							/>
+						</div>
+					</section>
+					<HeartVote
+						onSetValue={setValue}
+						onReset={resetState}
+						rating={currentBook.rating}
+					/>
+					<div className="relative mt-16 flex justify-center ">
+						<Image
+							src={upCat}
+							alt="playful cat"
+							width={60}
+							height={60}
+							className="absolute top-[-60px] scale-x-[-1] right-[30px] "
+						/>
+						<textarea
+							className=" bg-[#E4B781] placeholder-[#311C0D] placeholder-opacity-50 w-80 overflow-x-auto text-sm rounded-md"
+							rows={7}
+							id="resume"
+							placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum provident commodi in perspiciatis. Error laudantium ut minus architecto corrupti aut illum reiciendis velit, perferendis officia vero vel fuga nemo atque."
+							{...register("resume")}
+						></textarea>
+					</div>
+					<FormatChoice
 						register={register}
-						registerName="genre"
+						onReset={resetState}
+						error={errors}
+						currentFormat={currentBook.format.name}
+						currentDate={currentBook.date}
 					/>
-				</div>
-				<div>
-					<InputStyle
-						labelTxt="volume"
-						register={register}
-						registerName="volume"
-					/>
-					<InputStyle
-						labelTxt="serie"
-						register={register}
-						registerName="serie"
-					/>
-				</div>
-			</section>
-			<HeartVote
-				onSetValue={setValue}
-				onReset={resetState}
-				rating={currentBook.rating}
-			/>
-			<div className="relative mt-16 flex justify-center ">
-				<Image
-					src={upCat}
-					alt="playful cat"
-					width={60}
-					height={60}
-					className="absolute top-[-60px] scale-x-[-1] right-[30px] "
-				/>
-				<textarea
-					className=" bg-[#E4B781] placeholder-[#311C0D] placeholder-opacity-50 w-80 overflow-x-auto text-sm rounded-md"
-					rows={7}
-					id="resume"
-					placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum provident commodi in perspiciatis. Error laudantium ut minus architecto corrupti aut illum reiciendis velit, perferendis officia vero vel fuga nemo atque."
-					{...register("resume")}
-				></textarea>
-			</div>
-			<FormatChoice
-				register={register}
-				onReset={resetState}
-				error={errors}
-				currentFormat={currentBook.format.name}
-				currentDate={currentBook.date}
-			/>
-			<section className="flex justify-between mt-12">
-				<button
-					type="reset"
-					className="w-[150px] h-24 bg-[#E8CAA7] flex items-center justify-center mb-6"
-				>
-					<Image src={eraser} alt="reset" width={40} height={40} />
-				</button>
-				<button
-					type="submit"
-					className="w-[150px] h-24 bg-[#794822] flex items-center justify-center mb-6"
-				>
-					<Image src={check} alt="check" width={40} height={40} />
-				</button>
-			</section>
-		</form>
-	);
+					<section className="flex justify-between mt-12">
+						<button
+							type="reset"
+							className="w-[150px] h-24 bg-[#E8CAA7] flex items-center justify-center mb-6"
+						>
+							<Image src={eraser} alt="reset" width={40} height={40} />
+						</button>
+						<button
+							type="submit"
+							className="w-[150px] h-24 bg-[#794822] flex items-center justify-center mb-6"
+						>
+							<Image src={check} alt="check" width={40} height={40} />
+						</button>
+					</section>
+				</form>
+			</>
+		);
+	}
 };
 
 export default FormUpdate;
