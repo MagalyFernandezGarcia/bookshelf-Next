080d3d3501792057f018cafdfbbda9a151dd64b6
Magaly <fernandezgarciamagaly@gmail.com>
2024-11-27 11:43:51 +0100

    correcting filter issue that caused delay

--- app/bookshelf/page.tsx
+++ app/bookshelf/page.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 import ListOfBooks from "@/components/ListOfBooks";
 import {
-	byAuthor,
-	byFormat,
-	byGenre,
-	getAuthors,
-	getBooks,
-	getFormats,
-	getGenres,
-	searchAuthor,
-	searchBooks,
+  byAuthor,
+  byFormat,
+  byGenre,
+  getAuthors,
+  getBooks,
+  getFormats,
+  getGenres,
+  searchAuthor,
+  searchBooks,
 } from "../db.service";
 import { Author, Book } from "@prisma/client";
 import GeneralChoice from "@/components/GeneralChoice";
@@ -20,133 +20,138 @@ import Filter from "@/components/Filter";
 import sitCat from "@/images/sitCat.png";
 import ReclaimModal from "@/components/Modals/ReclaimModal";
 import {
-	getVisibilityReclaimModal,
-	deleteCookies,
+  getVisibilityReclaimModal,
+  deleteCookies,
 } from "@/actions/modal.action";
 
 const Page = async ({
-	searchParams,
+  searchParams,
 }: {
-	searchParams: {
-		filter: string;
-		author?: string;
-		genre?: string;
-		format?: string;
-		searchbar?: string;
-	};
+  searchParams: {
+    filter: string;
+    author?: string;
+    genre?: string;
+    format?: string;
+    searchbar?: string;
+  };
 }) => {
-	const filter = searchParams?.filter || "all";
-	let currentArray: Book[] = [];
-	let authors: Author[] = [];
-	let searchArray: Book[] = [];
-	const selectedAuthor = searchParams.author
-		? parseInt(searchParams.author, 10)
-		: undefined;
-	const selectedGenre = searchParams.genre
-		? parseInt(searchParams.genre, 10)
-		: undefined;
-	const selectedFormat = searchParams.format
-		? parseInt(searchParams.format, 10)
-		: undefined;
-	const searchBarValue = searchParams.searchbar;
+  const filter = searchParams?.filter || "all";
+  let currentArray: Book[] = [];
+  let authors: Author[] = [];
+  let searchArray: Book[] = [];
+  const selectedAuthor = searchParams.author
+    ? parseInt(searchParams.author, 10)
+    : undefined;
+  const selectedGenre = searchParams.genre
+    ? parseInt(searchParams.genre, 10)
+    : undefined;
+  const selectedFormat = searchParams.format
+    ? parseInt(searchParams.format, 10)
+    : undefined;
+  const searchBarValue = searchParams.searchbar;
 
-	const allBooks = await getBooks();
+  const allBooks = await getBooks();
 
-	switch (filter) {
-		case "present":
-			currentArray = allBooks.filter(
-				(book) => book.borrower === "" && book.returned === false
-			);
-			break;
-		case "absent":
-			currentArray = allBooks.filter(
-				(book) => book.borrower !== "" || book.returned === true
-			);
-			break;
-		case "author":
-			authors = await getAuthors();
-			break;
-		case "genre":
-			authors = await getGenres();
-			break;
-		case "rating":
-			currentArray = allBooks.sort((a: Book, b: Book) => b.rating - a.rating);
-			break;
-		case "format":
-			authors = await getFormats();
-			break;
-		case "all":
-			currentArray = allBooks;
-			break;
-		default:
-			currentArray = allBooks;
-	}
+  switch (filter) {
+    case "present":
+      currentArray = allBooks.filter(
+        (book) => book.borrower === "" && book.returned === false
+      );
+      break;
+    case "absent":
+      currentArray = allBooks.filter(
+        (book) => book.borrower !== "" || book.returned === true
+      );
+      break;
+    case "author":
+      authors = await getAuthors();
+      break;
+    case "genre":
+      authors = await getGenres();
+      break;
+    case "rating":
+      currentArray = allBooks.sort((a: Book, b: Book) => b.rating - a.rating);
+      break;
+    case "format":
+      authors = await getFormats();
+      break;
+    case "":
+    case "all":
+      currentArray = allBooks;
+      break;
+    default:
+      currentArray = allBooks;
+  }
 
-	if (selectedAuthor) {
-		searchArray = selectedAuthor ? await byAuthor(selectedAuthor) : [];
-	}
-	if (selectedGenre) {
-		searchArray = selectedGenre ? await byGenre(selectedGenre) : [];
-	}
-	if (selectedFormat) {
-		searchArray = selectedFormat ? await byFormat(selectedFormat) : [];
-	}
-	if (searchBarValue) {
-		currentArray = await searchBooks(searchBarValue);
-		authors = await searchAuthor(searchBarValue);
-	}
+  if (selectedAuthor) {
+    searchArray = selectedAuthor ? await byAuthor(selectedAuthor) : [];
+  }
+  if (selectedGenre) {
+    searchArray = selectedGenre ? await byGenre(selectedGenre) : [];
+  }
+  if (selectedFormat) {
+    searchArray = selectedFormat ? await byFormat(selectedFormat) : [];
+  }
+  if (searchBarValue) {
+    currentArray = await searchBooks(searchBarValue);
+    authors = await searchAuthor(searchBarValue);
+  }
 
-	const dateBorrow = allBooks.filter((book) => book.date);
-	const reclaim = dateBorrow.filter((book) => {
-		if (book.date) {
-			const diffDate = new Date(book.date).getTime() - new Date().getTime();
-			const diffInMonths = Math.floor(diffDate / (1000 * 60 * 60 * 24 * 30));
-			return diffInMonths < -6;
-		}
-		return false;
-	});
-	
+  const dateBorrow = allBooks.filter((book) => book.date);
+  const reclaim = dateBorrow.filter((book) => {
+    if (book.date) {
+      const diffDate = new Date(book.date).getTime() - new Date().getTime();
+      const diffInMonths = Math.floor(diffDate / (1000 * 60 * 60 * 24 * 30));
+      return diffInMonths < -6;
+    }
+    return false;
+  });
 
-	const modalIsVisible = await getVisibilityReclaimModal();
+  const modalIsVisible = await getVisibilityReclaimModal();
 
-	return (
-		<>
-			{modalIsVisible && reclaim.length !== 0 && <ReclaimModal array={reclaim} />}
-			
-			<SearchBar />
-			{reclaim.length > 0 && <form action={deleteCookies} className="mt-4">
-				<button className="bg-[#E4B781] p-2 text-center rounded-sm">
-					Prêtés
-				</button>
-			</form>}
-			
-			<Filter />
-			
-			<div className="relative">
-				{currentArray.length>0&&(<Image
-					src={sitCat}
-					alt="cat"
-					width={80}
-					height={80}
-					className="absolute right-10  top-[-40px] lg:right-20"
-				/>)}
-				
+  return (
+    <>
+      {modalIsVisible && reclaim.length !== 0 && (
+        <ReclaimModal array={reclaim} />
+      )}
 
-				{searchBarValue ? (
-					<>
-						<ListOfBooks currentArray={currentArray} />
-						<GeneralChoice valueChoice={authors} filter={filter} />
-					</>
-				) : selectedAuthor || selectedGenre || selectedFormat ? (
-					<ListOfBooks currentArray={searchArray} />
-				) : filter === "author" || filter === "genre" || filter === "format" ? (
-					<GeneralChoice valueChoice={authors} filter={filter} />
-				) : (
-					<ListOfBooks currentArray={currentArray} />
-				)}
-			</div>
-		</>
-	);
+      <SearchBar />
+      {reclaim.length > 0 && (
+        <form action={deleteCookies} className="mt-4">
+          <button className="bg-[#E4B781] p-2 text-center rounded-sm">
+            Prêtés
+          </button>
+        </form>
+      )}
+
+      <Filter />
+
+      <div className="relative">
+        {currentArray.length > 0 && (
+          <Image
+            src={sitCat}
+            alt="cat"
+            width={80}
+            height={80}
+            className="absolute right-10  top-[-40px] lg:right-20"
+          />
+        )}
+
+        {searchBarValue ? (
+          <>
+            <ListOfBooks currentArray={currentArray} />
+            <GeneralChoice valueChoice={authors} filter={filter} />
+          </>
+        ) : selectedAuthor || selectedGenre || selectedFormat ? (
+          <ListOfBooks currentArray={searchArray} />
+        ) : filter === "author" || filter === "genre" || filter === "format" ? (
+          <GeneralChoice valueChoice={authors} filter={filter} />
+        ) : (
+          <ListOfBooks currentArray={currentArray} />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Page;
--- components/Filter.tsx
+++ components/Filter.tsx
@@ -6,66 +6,56 @@ import add from "@/images/add.svg";
 
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
-
 const Filter = () => {
-	const searchParams = useSearchParams();
-	const { replace } = useRouter();
-	const pathName = usePathname();
-	const selectFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		const params = new URLSearchParams(searchParams);
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathName = usePathname();
+  const selectFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const params = new URLSearchParams(searchParams);
 
-		if (e) {
-			params.set("filter", e.target.value);
-		} else {
-			params.delete("filter");
-		}
-		replace(`${pathName}?${params.toString()}`);
-	};
+    if (e) {
+      params.delete("author");
+      params.delete("genre");
+      params.delete("format");
+      params.delete("searchbar");
+      params.set("filter", e.target.value);
+    } else {
+      params.delete("filter");
+    }
+    replace(`${pathName}?${params.toString()}`);
+  };
 
-	const handleFilterChange = (newFilter: string) => {
-		const params = new URLSearchParams(window.location.search);
-		params.delete("author");
-		params.delete("genre");
-		params.delete("format");
-		params.delete("searchbar");
-		params.set("filter", newFilter);
-		const newUrl = `${window.location.pathname}?${params.toString()}`;
-		window.location.href = newUrl;
-	};
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    selectFilter(e);
+  };
 
-	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		selectFilter(e);
-		handleFilterChange(e.target.value);
-	};
-
-	return (
-		<form
-			className="flex justify-between lg:mb-12"
-			onSubmit={(e) => e.preventDefault()}
-			
-		>
-			<select
-				name="filter"
-				onChange={handleChange}
-				className="bg-[#E4B781] mt-6 text-center rounded-sm lg:w-[200px] "
-			>
-				<option value="">Filtrer par :</option>
-				<option value="all">Tout</option>
-				<option value="author">Auteur</option>
-				<option value="genre">Genre</option>
-				<option value="format">Format</option>
-				<option value="rating">Avis</option>
-				<option value="absent">Absent</option>
-				<option value="present">Présent</option>
-			</select>
-			<Link
-				href="/"
-				className="flex gap-2 mt-6 bg-[#E4B781] p-2 rounded-sm justify-center items-center hover:scale-150"
-			>
-				<Image src={add} alt="plus" width={18} height={18} />
-			</Link>
-		</form>
-	);
+  return (
+    <form
+      className="flex justify-between lg:mb-12"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <select
+        name="filter"
+        onChange={handleChange}
+        className="bg-[#E4B781] mt-6 text-center rounded-sm lg:w-[200px] "
+      >
+        <option value="">Filtrer par :</option>
+        <option value="all">Tout</option>
+        <option value="author">Auteur</option>
+        <option value="genre">Genre</option>
+        <option value="format">Format</option>
+        <option value="rating">Avis</option>
+        <option value="absent">Absent</option>
+        <option value="present">Présent</option>
+      </select>
+      <Link
+        href="/"
+        className="flex gap-2 mt-6 bg-[#E4B781] p-2 rounded-sm justify-center items-center hover:scale-150"
+      >
+        <Image src={add} alt="plus" width={18} height={18} />
+      </Link>
+    </form>
+  );
 };
 
 export default Filter;
