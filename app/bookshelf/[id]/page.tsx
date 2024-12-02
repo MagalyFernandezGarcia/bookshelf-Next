3580bbc775dc89d628837d6233821d99c9b596ff
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-12-02 09:51:50 +0100

    modifying align of btn in page[id]

--- app/bookshelf/[id]/page.tsx
+++ app/bookshelf/[id]/page.tsx
@@ -10,144 +10,144 @@ import { dancingScript } from "@/app/fonts/fonts";
 import NavigateBtn from "@/components/Buttons/NavigateBtn";
 
 interface BookPageProps {
-  params: { id: string }; //params est un mot particulier de next qui permet de récupérer les queryparams, c'est pour ça que ça marche et pas la props id passée direct dans le component
+	params: { id: string }; //params est un mot particulier de next qui permet de récupérer les queryparams, c'est pour ça que ça marche et pas la props id passée direct dans le component
 }
 
 const BookPage = async ({ params }: BookPageProps) => {
-  const { id } = params;
+	const { id } = params;
 
-  const currentBook = await getFullBook(parseInt(id));
-  const arrayOfHearts: JSX.Element[] = [];
+	const currentBook = await getFullBook(parseInt(id));
+	const arrayOfHearts: JSX.Element[] = [];
 
-  function displayRating() {
-    if (currentBook) {
-      let count = 0;
+	function displayRating() {
+		if (currentBook) {
+			let count = 0;
 
-      for (let i = 0; i < currentBook.rating; i++) {
-        arrayOfHearts[i] = (
-          <svg
-            className="fill-[#D8778D] text-red-400 w-6 h-auto"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            key={count++}
-          >
-            <path
-              d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"
-              style={{ stroke: "#311C0D", strokeWidth: 20 }}
-            />
-          </svg>
-        );
-      }
-      while (arrayOfHearts.length < 5) {
-        arrayOfHearts.push(
-          <svg
-            className="fill-white text-red-400 w-6 h-auto"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            key={count++}
-          >
-            <path
-              d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"
-              style={{ stroke: "#311C0D", strokeWidth: 20 }}
-            />
-          </svg>
-        );
-      }
-    }
+			for (let i = 0; i < currentBook.rating; i++) {
+				arrayOfHearts[i] = (
+					<svg
+						className="fill-[#D8778D] text-red-400 w-6 h-auto"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512 512"
+						key={count++}
+					>
+						<path
+							d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"
+							style={{ stroke: "#311C0D", strokeWidth: 20 }}
+						/>
+					</svg>
+				);
+			}
+			while (arrayOfHearts.length < 5) {
+				arrayOfHearts.push(
+					<svg
+						className="fill-white text-red-400 w-6 h-auto "
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512 512"
+						key={count++}
+					>
+						<path
+							d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"
+							style={{ stroke: "#311C0D", strokeWidth: 20 }}
+						/>
+					</svg>
+				);
+			}
+		}
 
-    return arrayOfHearts;
-  }
+		return arrayOfHearts;
+	}
 
-  return (
-    <>
-      {currentBook ? (
-        <>
-          <NavigateBtn
-            location="left"
-            txt="Retour"
-            href="/bookshelf"
-            retainQueryParams
-          />
-          <section className="flex flex-col items-center mt-4 lg:mb-12">
-            <h1
-              className={`${dancingScript.className} text-3xl font-bold lg:mb-12`}
-            >
-              {currentBook.title}
-            </h1>
-            <section className="flex flex-col mt-6 w-[320px] ">
-              <div className="flex justify-between ">
-                <p>{currentBook.author.name}</p>
-                <p>{currentBook.genre.name}</p>
-              </div>
-              {currentBook.serie ? (
-                <div className="flex  justify-between mt-2 lg:mt-6 ">
-                  <p>{currentBook.serie.name}</p> <p>{currentBook.volume}</p>
-                </div>
-              ) : null}
-            </section>
-            <section className="mt-8 flex gap-2 lg:mt-16">
-              {" "}
-              {displayRating()}
-            </section>
-          </section>
-          <section className="mt-8 bg-[#E4B781] bg-opacity-70 w-[320px] ] ml-4 h-fit p-4 lg:w-[600px]">
-            {currentBook.resume}
-          </section>
-          <div className="flex justify-center mt-6">
-            {" "}
-            {currentBook.borrower !== "" || currentBook.returned === true ? (
-              <p className=" text-[#E8410E] lg:mt-8 ">
-                Absent de la bibliothèque
-              </p>
-            ) : null}
-          </div>
-          <section className=" flex flex-col items-start ml-4 lg:mt-8">
-            <p className="mt-8">Format : {currentBook.format.name}</p>{" "}
-            {currentBook.borrower && (
-              <>
-                <p className="mt-2 lg:mt-4">
-                  Emprunte par {currentBook.borrower}{" "}
-                </p>
-                {currentBook.date && (
-                  <p className="mt-2 lg:mt-4">
-                    Prêté le :{" "}
-                    {currentBook.date.toLocaleDateString("fr-FR", {
-                      day: "numeric",
-                      month: "numeric",
-                      year: "numeric",
-                    })}
-                  </p>
-                )}
-                <Switch currentBook={currentBook} />
-              </>
-            )}
-          </section>
-          <section className=" flex gap-2 mt-12 absolute right-4 ">
-            <div className="p-2 bg-[#E4B781] rounded-sm flex justify-center items-center relative lg:w-[40px]">
-              <UpdateBtn sizeIcon={16} id={currentBook.id} />
-            </div>
-            <div className="p-2 bg-[#E4B781] rounded-sm flex justify-center items-center relative lg:w-[40px]">
-              <DeleteBtn id={currentBook.id} sizeIcon={16} />
-            </div>
-            <Link
-              href="/"
-              className="bg-[#E4B781] p-2 rounded-sm flex justify-center items-center "
-            >
-              <Image
-                src={add}
-                alt="plus"
-                width={18}
-                height={18}
-                className="lg:w-[30px] hover:scale-150"
-              />
-            </Link>
-          </section>
-        </>
-      ) : (
-        <p className="text-4xl mt-44">Book not found</p>
-      )}
-    </>
-  );
+	return (
+		<>
+			{currentBook ? (
+				<>
+					<NavigateBtn
+						location="left"
+						txt="Retour"
+						href="/bookshelf"
+						retainQueryParams
+					/>
+					<section className="flex flex-col items-center mt-4 lg:mb-12">
+						<h1
+							className={`${dancingScript.className} text-3xl font-bold lg:mb-12`}
+						>
+							{currentBook.title}
+						</h1>
+						<section className="flex flex-col mt-6 w-[320px] ">
+							<div className="flex justify-between ">
+								<p>{currentBook.author.name}</p>
+								<p>{currentBook.genre.name}</p>
+							</div>
+							{currentBook.serie ? (
+								<div className="flex  justify-between mt-2 lg:mt-6 ">
+									<p>{currentBook.serie.name}</p> <p>{currentBook.volume}</p>
+								</div>
+							) : null}
+						</section>
+						<section className="mt-8 flex gap-2 lg:mt-16">
+							{" "}
+							{displayRating()}
+						</section>
+					</section>
+					<section className="mt-8 bg-[#E4B781] bg-opacity-70 w-[320px] ] ml-4 h-fit p-4 lg:w-[600px]">
+						{currentBook.resume}
+					</section>
+					<div className="flex justify-center mt-6">
+						{" "}
+						{currentBook.borrower !== "" || currentBook.returned === true ? (
+							<p className=" text-[#E8410E] lg:mt-8 ">
+								Absent de la bibliothèque
+							</p>
+						) : null}
+					</div>
+					<section className=" flex flex-col items-start ml-4 lg:mt-8">
+						<p className="mt-8">Format : {currentBook.format.name}</p>{" "}
+						{currentBook.borrower && (
+							<>
+								<p className="mt-2 lg:mt-4">
+									Emprunte par {currentBook.borrower}{" "}
+								</p>
+								{currentBook.date && (
+									<p className="mt-2 lg:mt-4">
+										Prêté le :{" "}
+										{currentBook.date.toLocaleDateString("fr-FR", {
+											day: "numeric",
+											month: "numeric",
+											year: "numeric",
+										})}
+									</p>
+								)}
+								<Switch currentBook={currentBook} />
+							</>
+						)}
+					</section>
+					<section className=" flex gap-2 mt-12 absolute right-4 bottom-2 ">
+						<div className="p-2 bg-[#E4B781] rounded-sm flex justify-center items-center relative lg:w-[40px]">
+							<UpdateBtn sizeIcon={16} id={currentBook.id} />
+						</div>
+						<div className="p-2 bg-[#E4B781] rounded-sm flex justify-center items-center relative lg:w-[40px]">
+							<DeleteBtn id={currentBook.id} sizeIcon={16} />
+						</div>
+						<Link
+							href="/"
+							className="bg-[#E4B781] p-2 rounded-sm flex justify-center items-center "
+						>
+							<Image
+								src={add}
+								alt="plus"
+								width={18}
+								height={18}
+								className="lg:w-[30px] hover:scale-150"
+							/>
+						</Link>
+					</section>
+				</>
+			) : (
+				<p className="text-4xl mt-44">Book not found</p>
+			)}
+		</>
+	);
 };
 
 export default BookPage;
--- components/Filter.tsx
+++ components/Filter.tsx
@@ -9,60 +9,60 @@ import { useState } from "react";
 import Spinner from "./Spinner";
 
 const Filter = () => {
-  const searchParams = useSearchParams();
-  const { replace } = useRouter();
-  const pathName = usePathname();
-  const selectFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const params = new URLSearchParams(searchParams);
+	const searchParams = useSearchParams();
+	const { replace } = useRouter();
+	const pathName = usePathname();
+	const selectFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const params = new URLSearchParams(searchParams);
 
-    if (e) {
-      params.delete("author");
-      params.delete("genre");
-      params.delete("format");
-      params.delete("searchbar");
-      params.delete("sort");
-      params.delete("serie");
-      params.set("filter", e.target.value);
-    } else {
-      params.delete("filter");
-    }
-    replace(`${pathName}?${params.toString()}`);
-  };
-  const [isLoading, setIsLoading] = useState(false);
+		if (e) {
+			params.delete("author");
+			params.delete("genre");
+			params.delete("format");
+			params.delete("searchbar");
+			params.delete("sort");
+			params.delete("serie");
+			params.set("filter", e.target.value);
+		} else {
+			params.delete("filter");
+		}
+		replace(`${pathName}?${params.toString()}`);
+	};
+	const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    selectFilter(e);
-    setIsLoading(true);
-    setTimeout(() => setIsLoading(false), 1400);
-  };
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		selectFilter(e);
+		setIsLoading(true);
+		setTimeout(() => setIsLoading(false), 1400);
+	};
 
-  if (isLoading) return <Spinner size={40} />;
+	if (isLoading) return <Spinner size={40} />;
 
-  return (
-    <form
-      className="flex justify-between lg:mb-12 items-end"
-      onSubmit={(e) => e.preventDefault()}
-    >
-      <select
-        name="filter"
-        onChange={handleChange}
-        className="bg-[#E4B781] mt-6 text-center rounded-sm lg:w-[200px] p-2 "
-      >
-        <option value="">Filtrer par :</option>
-        <option value="all">Tout</option>
-        <option value="absent">Absent</option>
-        <option value="present">Présent</option>
-        <option value="lend">Prêtés</option>
-      </select>
+	return (
+		<form
+			className="flex justify-between lg:mb-12 items-end"
+			onSubmit={(e) => e.preventDefault()}
+		>
+			<select
+				name="filter"
+				onChange={handleChange}
+				className="bg-[#E4B781] mt-6  rounded-sm lg:w-[200px] p-2 text-center "
+			>
+				<option value="">Filtrer par :</option>
+				<option value="all">Tout</option>
+				<option value="absent">Absent</option>
+				<option value="present">Présent</option>
+				<option value="lend">Prêtés</option>
+			</select>
 
-      <Link
-        href="/"
-        className="flex gap-2 mt-6 bg-[#E4B781] p-2 rounded-sm justify-center items-center hover:scale-150"
-      >
-        <Image src={add} alt="plus" width={18} height={18} />
-      </Link>
-    </form>
-  );
+			<Link
+				href="/"
+				className="flex gap-2 mt-6 bg-[#E4B781] p-2 rounded-sm justify-center items-center hover:scale-150"
+			>
+				<Image src={add} alt="plus" width={18} height={18} />
+			</Link>
+		</form>
+	);
 };
 
 export default Filter;
